880c2006f2e223bce2c9d6dec789e7f3b5da5d86
Ferhan ABACI <ferhan.abaci@santsg.com>
2023-03-28 23:32:31 +0300

    Controller and Tema Added

--- ApiConsume/HotelProject.BusinessLayer/Concrete/StaffManager.cs
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/StaffManager.cs
@@ -1,4 +1,5 @@
 ﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
 using HotelProject.EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,36 +11,34 @@ namespace HotelProject.BusinessLayer.Concrete
 {
     public class StaffManager : IStaffService
     {
-        private readonly IStaffService _staffService;
-
-        public StaffManager(IStaffService staffService)
+        private readonly IStaffDal _staffDal;
+        public StaffManager(IStaffDal staffDal)
         {
-            _staffService = staffService;
+            _staffDal = staffDal;
         }
-
         public void TDelete(Staff t)
         {
-            _staffService.TDelete(t);
+            _staffDal.Delete(t);
         }
 
         public Staff TGetByID(int id)
         {
-            return _staffService.TGetByID(id);
+            return _staffDal.GetByID(id);
         }
 
         public List<Staff> TGetList()
         {
-            return _staffService.TGetList();
+            return _staffDal.GetList();
         }
 
         public void TInsert(Staff t)
         {
-            _staffService.TInsert(t);
+            _staffDal.Insert(t);
         }
 
         public void TUpdate(Staff t)
         {
-            _staffService.TUpdate(t);
+            _staffDal.Update(t);
         }
     }
 }
