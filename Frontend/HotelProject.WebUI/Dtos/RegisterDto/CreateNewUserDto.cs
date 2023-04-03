2bcb5184d3e24af33daacca734bbfbe691db58e2
FerhanAbaci <ferhanabaci@hotmail.com>
2023-04-04 00:05:03 +0300

    Register And  Login

---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage ="Ad Alanı Gereklidir")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Surname Alanı Gereklidir")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı Alanı Gereklidir")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Mail Alanı Gereklidir")]
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Şifre Alanı Gereklidir")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Şifre Tekrar Alanı Gereklidir")]
+        [Compare("Password",ErrorMessage ="Lütfen ilk şifre ile aynı şifreyi girin")]
+        public string ConfirmPassword { get; set; }
+    }
+}
