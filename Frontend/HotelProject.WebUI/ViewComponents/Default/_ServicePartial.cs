937f861d61af7424db1e8da903fd8cf2ddab9c0c
FerhanAbaci <ferhanabacii@gmail.com>
2023-06-19 23:37:39 +0300

    launchSettings edit

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
@@ -18,7 +18,7 @@ namespace HotelProject.WebUI.ViewComponents.Default
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("http://localhost:10439/api/Service");
+            var responseMessage = await client.GetAsync("http://localhost:5000/api/Service");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
